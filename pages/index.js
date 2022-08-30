9342d7fad091a39cbae6531649e64bcc55da480f
garrettprince <89549339+garrettprince@users.noreply.github.com>
2022-08-29 21:03:31 -0700

    Added component to test pulling data

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
-
 import Post from '../components/Post'
+import supabase from '../utils/supabase'
 
-export function getStaticProps() {
+export async function getStaticProps() {
+
+  const { data: posts, error } = await supabase.from('posts').select('*')
+
+  if (error) {
+    throw new Error(error)
+  }
+
   return {
     props: {
-      posts: []
+      posts,
     }
   }
 }
@@ -14,6 +21,7 @@ export default function Home({ posts }) {
     <div>
       <h1>Hello test</h1>
       <pre>{JSON.stringify(posts, null, 2)}</pre>
+      <h2>{posts.title}</h2>
     </div>
   )
 }
